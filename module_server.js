e57558fbd10c1e395ece80acf5412ea050a520e5
UltimateHikari <votemodur@gmail.com>
2018-11-26 18:43:29 +0700

    teamnames extraction

--- module_server.js
+++ module_server.js
@@ -14,6 +14,7 @@ function module_getParams(id){
 }
 
 function module_getTeamnames(id){
+	var repacked_teamArray = [];
 	var maxTeams = module_getParams(id).team;
 	var teamArray = GetSheet(TEAMS, id).getRange(1, 1, maxTeams).getValues();
 	for(i in teamArray){
